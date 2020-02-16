ca8333d471350b9cb37c4446614ad93fc4c728dc
danielhamilton <dh@danielhamilton.com>
2020-02-16 00:26:31 -0500

    Graph updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 20, left: 30},
-  width = 450 - margin.left - margin.right,
-  height = 300 - margin.top - margin.bottom;
+var margin = {top: 10, right: 50, bottom: 50, left: 60},
+  width = 1200 - margin.left - margin.right,
+  height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -13,47 +13,36 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Read dummy data
-d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json", function( data) {
+d3.json("js/role-permission-data.json", function( data) {
 
   // List of node names
   var allNodes = data.nodes.map(function(d){return d.name})
+
+  // List of groups
+  var allGroups = data.nodes.map(function(d){return d.grp})
+  allGroups = [...new Set(allGroups)]
+
+  // A color scale for groups:
+  var color = d3.scaleOrdinal()
+    .domain(allGroups)
+    .range(d3.schemeSet3);
+
+  // A linear scale for node size
+  var size = d3.scaleLinear()
+    .domain([1,10])
+    .range([2,10]);
 
   // A linear scale to position the nodes on the X axis
   var x = d3.scalePoint()
     .range([0, width])
     .domain(allNodes)
 
-  // Add the circle for the nodes
-  var nodes = svg
-    .selectAll("mynodes")
-    .data(data.nodes)
-    .enter()
-    .append("circle")
-      .attr("cx", function(d){ return(x(d.name))})
-      .attr("cy", height-30)
-      .attr("r", 8)
-      .style("fill", "#69b3a2")
-      </script>
-
-  // And give them a label
-  var labels = svg
-    .selectAll("mylabels")
-    .data(data.nodes)
-    .enter()
-    .append("text")
-      .attr("x", function(d){ return(x(d.name))})
-      .attr("y", height-10)
-      .text(function(d){ return(d.name)})
-      .style("text-anchor", "middle")
-
-  // Add links between nodes. Here is the tricky part.
   // In my input data, links are provided between nodes -id-, NOT between node names.
   // So I have to do a link between this id and the name
   var idToNode = {};
   data.nodes.forEach(function (n) {
     idToNode[n.id] = n;
   });
-  // Cool, now if I do idToNode["2"].name I've got the name of the node with id 2
 
   // Add the links
   var links = svg
@@ -72,33 +61,60 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/d
         .join(' ');
     })
     .style("fill", "none")
-    .attr("stroke", "black")
+    .attr("stroke", "grey")
+    .style("stroke-width", 1)
 
-    // Add the highlighting functionality
-    nodes
-      .on('mouseover', function (d) {
-        // Highlight the nodes: every node is green except of him
-        nodes.style('fill', "#B8B8B8")
-        d3.select(this).style('fill', '#69b3b2')
-        // Highlight the connections
-        links
-          .style('stroke', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? '#69b3b2' : '#b8b8b8';})
-          .style('stroke-width', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? 4 : 1;})
-      })
-      .on('mouseout', function (d) {
-        nodes.style('fill', "#69b3a2")
-        links
-          .style('stroke', 'black')
-          .style('stroke-width', '1')
-      })
+  // Add the circle for the nodes
+  var nodes = svg
+    .selectAll("mynodes")
+    .data(data.nodes.sort(function(a,b) { return +b.n - +a.n }))
+    .enter()
+    .append("circle")
+      .attr("cx", function(d){ return(x(d.name))})
+      .attr("cy", height-30)
+      .attr("r", function(d){ return(size(d.n))})
+      .style("fill", function(d){ return color(d.grp)})
+      .attr("stroke", "white")
+
+  // And give them a label
+  var labels = svg
+    .selectAll("mylabels")
+    .data(data.nodes)
+    .enter()
+    .append("text")
+      .attr("x", 0)
+      .attr("y", 0)
+      .text(function(d){ return(d.name)} )
+      .style("text-anchor", "end")
+      .attr("transform", function(d){ return( "translate(" + (x(d.name)) + "," + (height-15) + ")rotate(-45)")})
+      .style("font-size", 6)
+
+  // Add the highlighting functionality
+  nodes
+    .on('mouseover', function (d) {
+      // Highlight the nodes: every node is green except of him
+      nodes
+        .style('opacity', .2)
+      d3.select(this)
+        .style('opacity', 1)
+      // Highlight the connections
+      links
+        .style('stroke', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? color(d.grp) : '#b8b8b8';})
+        .style('stroke-opacity', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? 1 : .2;})
+        .style('stroke-width', function (link_d) { return link_d.source === d.id || link_d.target === d.id ? 4 : 1;})
+      labels
+        .style("font-size", function(label_d){ return label_d.name === d.name ? 16 : 2 } )
+        .attr("y", function(label_d){ return label_d.name === d.name ? 10 : 0 } )
+
+    })
+    .on('mouseout', function (d) {
+      nodes.style('opacity', 1)
+      links
+        .style('stroke', 'grey')
+        .style('stroke-opacity', .8)
+        .style('stroke-width', '1')
+      labels
+        .style("font-size", 6 )
+
+    })
 })
-
-// text hover nodes
-svg
-  .append("text")
-    .attr("text-anchor", "middle")
-    .style("fill", "#B8B8B8")
-    .style("font-size", "17px")
-    .attr("x", 50)
-    .attr("y", 10)
-    .html("Hover nodes")
