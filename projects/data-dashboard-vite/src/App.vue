4b5dd558410d342ae616827f074040d1eef4c106
danielhamilton <307828+danielhamilton@users.noreply.github.com>
2021-10-11 23:58:53 -0400

    vite update

--- projects/data-dashboard-vite/src/App.vue
+++ projects/data-dashboard-vite/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import PeopleCard from "./components/PeopleCard.vue";
 import Dashboard1 from "./components/Dashboard-1.vue";
 </script>
 
 <template>
- 
   <Dashboard1/>
 </template>
