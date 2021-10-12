a51873cc1ec189cb7af883006b42c54739b4a3ea
danielhamilton <307828+danielhamilton@users.noreply.github.com>
2021-10-11 23:47:13 -0400

    tailwind and vue dashboard practice

---
+++ projects/data-dashboard-vite/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PeopleCard from "./components/PeopleCard.vue";
+import Dashboard1 from "./components/Dashboard-1.vue";
+</script>
+
+<template>
+ 
+  <Dashboard1/>
+</template>
